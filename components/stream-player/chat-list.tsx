ac0e48e7aa2ce60cb0cf307326b99fa0fe0a7646
raz228822 <raz228822@gmail.com>
2025-04-24 20:32:15 +0300

    Community component

--- components/stream-player/chat-list.tsx
+++ components/stream-player/chat-list.tsx
@@ -1,5 +1,6 @@
 import { ReceivedChatMessage } from "@livekit/components-react";
 import { ChatMessage } from "./chat-message";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface ChatListProps {
     messages: ReceivedChatMessage[];
@@ -28,6 +29,14 @@ export const ChatList = ({
                     data={message}
                 />
             ))}
+        </div>
+    )
+}
+
+export const ChatListSkeletoon = () => {
+    return (
+        <div className="flex h-full items-center justify-center">
+            <Skeleton className="w-1/2 h-6" />
         </div>
     )
 }
