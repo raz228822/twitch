7ea34f6b4081e829c9b71add15045af39619bf3b
raz228822 <raz228822@gmail.com>
2025-05-08 16:09:52 +0300

    Unblock page

---
+++ app/(dashboard)/u/[username]/community/_components/unblock-button.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { toast } from "sonner";
+import { useTransition } from "react";
+
+import { onUnBlock } from "@/actions/block";
+import { Button } from "@/components/ui/button";
+
+interface UnblockButtonProps {
+    userId: string;
+};
+
+export const UnBlockButton = ({
+    userId,
+}: UnblockButtonProps) => {
+    const [isPending, startTransition] = useTransition();
+
+    const onClick = () => {
+        startTransition(() => {
+            onUnBlock(userId)
+                .then((result) => toast.success(`User ${result.blocked.username} unblocked`))
+                .catch((result) => toast.error("Something went wrong"))
+        });
+    };
+
+    return (
+        <Button
+            disabled={isPending}
+            onClick={onClick}
+            variant="link"
+            size="sm"
+            className="text-blue-500 w-full"
+        >
+            Unblock
+        </Button>
+    )
+}
